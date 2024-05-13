22c8871c421a263863906a0d804239f085b3cad7
Broyler <69675212+Broyler@users.noreply.github.com>
2024-05-13 04:16:04 +0300

    Отображать ползунки

--- main.c
+++ main.c
@@ -6,14 +6,12 @@
 #include <stdint.h>
 #include <stdio.h>
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-const SDL_Color backgroundColor = {0x3b, 0x3e, 0x4f, 0xff};
-const SDL_Color triangleColors[3] = {
-	{0xff, 0, 0, 0xff},
-	{0, 0, 0xff, 0xff},
-	{0, 0xff, 0, 0xff}
-};
+typedef struct {
+	const int index;
+	const double min;
+	const double max;
+	double* value;
+} Slider;
 
 bool run = true;
 double speed = M_PI / 2;  // Обороты в секунду
@@ -21,6 +19,23 @@ double radius = 100;
 double rotation;
 uint64_t frequency;
 SDL_Vertex vertices[3];
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const SDL_Color BACKGROUND_COLOR = {0x3b, 0x3e, 0x4f, 0xff};
+const SDL_Color TRIANGLE_COLORS[3] = {
+	{0xff, 0, 0, 0xff},
+	{0, 0, 0xff, 0xff},
+	{0, 0xff, 0, 0xff}
+};
+const int SLIDER_PADDING = 150;
+const int SLIDER_OFFSET = 50;
+const int SLIDER_WIDTH = 5;
+const int HANDLE_WIDTH = 13;
+const SDL_Color SLIDER_COLOR = {0, 0, 0, 0xff};
+const SDL_Color HANDLE_COLOR = {0xff, 0xff, 0xff, 0xff};
+Slider radiusSlider = {1, 10, 250, &radius};
+Slider speedSlider = {2, -2 * M_PI, 2 * M_PI, &speed};
 
 SDL_Window* InitSDL() {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -37,6 +52,59 @@ SDL_Window* InitSDL() {
     );
 }
 
+double InverseLerp(double x, double min, double max) {
+	return (x - min) / (max - min);
+}
+
+void RenderSliderBars(SDL_Renderer* renderer) {
+	SDL_SetRenderDrawColor(
+		renderer,
+		SLIDER_COLOR.r,
+		SLIDER_COLOR.g,
+		SLIDER_COLOR.b,
+		SLIDER_COLOR.a
+	);
+
+	for (int i = 1; i < 3; ++i) {
+		SDL_RenderFillRect(renderer, &(SDL_Rect){
+			SLIDER_PADDING,
+			SCREEN_HEIGHT - SLIDER_OFFSET * i,
+			SCREEN_WIDTH - 2 * SLIDER_PADDING,
+			SLIDER_WIDTH
+		});
+	}
+}
+
+void RenderHandle(SDL_Renderer* renderer, Slider* slider) {
+	double percentage = InverseLerp(*(slider->value), slider->min, slider->max);
+	int x = percentage * (SCREEN_WIDTH - 2 * SLIDER_PADDING) + SLIDER_PADDING;
+	int y = SCREEN_HEIGHT - SLIDER_OFFSET * slider->index;
+	y -= (HANDLE_WIDTH - SLIDER_WIDTH) / 2;
+	SDL_RenderFillRect(renderer, &(SDL_Rect){
+		x,
+		y,
+		HANDLE_WIDTH,
+		HANDLE_WIDTH
+	});
+}
+
+void RenderSliderHandles(SDL_Renderer* renderer) {
+	SDL_SetRenderDrawColor(
+		renderer,
+		HANDLE_COLOR.r,
+		HANDLE_COLOR.g,
+		HANDLE_COLOR.b,
+		HANDLE_COLOR.a
+	);
+	RenderHandle(renderer, &radiusSlider);
+	RenderHandle(renderer, &speedSlider);
+}
+
+void RenderSliders(SDL_Renderer* renderer) {
+	RenderSliderBars(renderer);
+	RenderSliderHandles(renderer);
+}
+
 void CalculateVertices() {
 	double angle, x, y;
 
@@ -44,7 +112,7 @@ void CalculateVertices() {
 		angle = rotation + i * (2 * M_PI / 3);
 		x = cos(angle) * radius + SCREEN_WIDTH / 2;
 		y = sin(angle) * radius + SCREEN_HEIGHT / 2;
-		vertices[i] = (SDL_Vertex){(SDL_FPoint){x, y}, triangleColors[i]};
+		vertices[i] = (SDL_Vertex){(SDL_FPoint){x, y}, TRIANGLE_COLORS[i]};
 	}
 }
 
@@ -59,10 +127,17 @@ void EventLoop(SDL_Event* e) {
 	}
 }
 
-void Render(SDL_Renderer* renderer, const SDL_Color* bg) {
-	SDL_SetRenderDrawColor(renderer, bg->r, bg->g, bg->b, bg->a);
+void Render(SDL_Renderer* renderer) {
+	SDL_SetRenderDrawColor(
+		renderer,
+		BACKGROUND_COLOR.r,
+		BACKGROUND_COLOR.g,
+		BACKGROUND_COLOR.b,
+		BACKGROUND_COLOR.a
+	);
 	SDL_RenderClear(renderer);
 	SDL_RenderGeometry(renderer, NULL, vertices, 3, NULL, 0);
+	RenderSliders(renderer);
 	SDL_RenderPresent(renderer);
 }
 
@@ -93,7 +168,7 @@ int main(int argc, char* argv[]) {
     	EventLoop(&e);
     	CalculateVertices();
     	AddRotation(&lastTick);
-    	Render(renderer, &backgroundColor);
+    	Render(renderer);
     }
     
     SDL_DestroyWindow(window);
